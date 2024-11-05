908bfbd97b4910fdcb697b257744626a7a5b07d5
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-11-05 16:14:40 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const app= express();
 
+
+// app.use("/user", (req,res) => {
+    
+//     res.send("ORDER MATTERS");
+   
+// });
+
+
+//This will handle only get call to user
+app.get("/user", (req,res) => {
+    res.send({firstname:"Kaustubh" , lastname:"Sawant"});
+   
+});
+
+
+app.post("/user", (req,res) => {
+    //Saving data to DB
+    res.send("Data succesfully save dto Database");
+   
+});
+
+app.delete("/user", (req,res) => {
+    
+    res.send("Deleted successfuully");
+   
+});
+
  //always write bigger same routes first
 app.use("/hello/2", (req,res) => {
     res.send("hello 2 2 2 2 2  2 2 2");
